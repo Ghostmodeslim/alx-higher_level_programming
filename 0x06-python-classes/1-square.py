cd50c7cbebb2b7cab2d678d49ef0121ded819ab8
Ehimwenma idemudia <ehimwenma.idemudia@outlook.com>
2022-09-13 19:49:59 +0000

    square size

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Define an empty class"""
+
+
+class Square:
+    ''' a class named square'''
+    __size = None
+
+    def __init__(self, size):
+        '''Initializes class'''
+        self.__size = 
