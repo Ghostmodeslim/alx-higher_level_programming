e14559fedc1136757d51493d1bac34a6a02fa964
Ehimwenma <ehimwenma.idemudia@outlook.com>
2022-11-03 16:57:25 -0700

    fix

--- 0x0E-SQL_more_queries/14-my_genres.sql
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -1,7 +1,18 @@
--- List all genres of the show, Dexter
-SELECT tv_genres.name FROM tv_genres
+-- https://s3.amazonaws.com/intranet-projects-files/holbertonschool-higher-level_programming+/274/hbtn_0d_tvshows.sql
+-- run to import SQL dump:
+--     * echo "CREATE DATABASE hbtn_0d_tvshows;" | mysql -uroot -p
+--     * curl [link] -s | mysql -uroot -p hbtn_0d_tvshows
+--
+-- lists all shows contained in hbtn_0d_tvshows genres of the show "Dexter"
+-- each record should display: tv_genres.name
+-- results must be sorted in ascending order by genre name
+-- only one SELECT statement
+
+SELECT tv_genres.name
+FROM tv_genres
 INNER JOIN tv_show_genres
+ON tv_genres.id = tv_show_genres.genre_id
 INNER JOIN tv_shows
-ON tv_genres.id=tv_show_genres.genre_id
-WHERE tv_genres.id = tv_show_genres.genre_id AND tv_shows.title="Dexter" AND tv_show_genres.show_id=tv_shows.id
-GROUP BY tv_genres.name;
+ON tv_show_genres.show_id = tv_shows.id
+WHERE tv_shows.title = "Dexter"
+ORDER BY tv_genres.name ASC;
